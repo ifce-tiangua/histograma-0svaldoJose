5d9f4d3403b07df72f1e5908b5333d06a698945d
Osvaldo <107486317+0svaldoJose@users.noreply.github.com>
2024-11-21 13:09:43 -0300

    02

--- main.c
+++ main.c
@@ -1,53 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void preencher(char* his[], int valor, int categoria );
-void Imprimir_Histograma(char* his[], int valor[], int categoria);
-
-int main(){
-    
-    int qtd_categ, i;
-    scanf("%d", &qtd_categ);
-    
-    char *histograma[qtd_categ];
-    int vendasPorCateg[qtd_categ];
-    
-    for(i=0; i<qtd_categ; i++){
-        scanf("%d", &vendasPorCateg[i]);
-        if(vendasPorCateg[i] == 0){
-            histograma[i] = NULL;
-        }
-        else{
-            histograma[i] = (char *) malloc(vendasPorCateg[i]* sizeof(char));
-            preencher(histograma, vendasPorCateg[i], i); 
-        }
-    }
-    Imprimir_Histograma(histograma, vendasPorCateg, qtd_categ); 
-    for(i=0; i<qtd_categ; i++){
-        free(histograma[i]);
-    }
-    return 0;
-}
-void preencher(char* his[], int valor, int categoria){
-
-    int i;
-
-    for(i=0; i<valor; i++){
-        his[categoria][i] = '*';
+void preenche_histograma(char *h[], int valor, int categoria){
+    for(int i=0; i<valor; i++){
+        h[categoria][i]= '*';
     }
 }
-void Imprimir_Histograma(char* his[], int valor[], int categoria){
-    int i, j;
-    if(categoria==0){
-        printf("vazio\n");
+void imprime_histograma(char *h[], int valores[], int categorias){
+    if(categorias==0){
+        printf("vazio");
+        return;
     }
-    for(i=0; i<categoria; i++){
-        printf("%d ", valor[i]);
-        if(valor[i] != 0){
-            for(j=0; j<valor[i]; j++){
-                printf("%c", his[i][j]);
-            }
+    for(int i=0; i<categorias; i++){
+        printf("%d ", valores[i]);
+        for(int j=0; j<valores[i]; j++){
+            printf("%c", h[i][j]);
         }
         printf("\n");
     }
+
+}
+
+int main(){
+  int categorias;
+  scanf("%d", &categorias);
+  
+  int valores[categorias];
+  char* histograma[categorias];
+  for(int i=0;i<categorias;i++){
+    scanf("%d",&valores[i]);
+    if(valores[i]==0){
+      histograma[i]=NULL;  
+    }else{
+        histograma[i]= (char*)malloc(valores[i]*sizeof(char));
+        preenche_histograma(histograma, valores[i], i);
+    }
+    
+  }
+   imprime_histograma(histograma,valores,categorias);
+
+   for(int i=0; i<categorias;i++){
+    free(histograma[i]);
+   }
+    return 0;
 }
